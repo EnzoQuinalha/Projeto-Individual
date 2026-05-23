b5bf72bb4e7894ce9b25198e5ac5d0e7313dd690
Enzo Quinlha <enzoquinalha31@gmail.com>
2026-05-23 12:12:09 -0300

    Finalizando Dashboard, renomeando imagens e ajustando models

--- src/models/tierlistModel.js
+++ src/models/tierlistModel.js
@@ -14,7 +14,7 @@ function publicar(idUsuario,idBoss,notaTier){
 function listar(idUsuario){
     console.log("ACESSEI O TIERLIST MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", idUsuario);
     var instrucaoSql = `
-        SELECT u.id, u.nome as usuario, b.nome as boss, avat.nota FROM usuario u JOIN avaliacoes_tierlist avat ON avat.id_usuario = u.id JOIN bosses b ON b.id = avat.id_boss WHERE u.id=${idUsuario};
+        SELECT u.id, u.nome as usuario, b.nome as boss, b.tag, ROUND(AVG(avat.nota), 1) as nota FROM usuario u JOIN avaliacoes_tierlist avat ON avat.id_usuario = u.id JOIN bosses b ON b.id = avat.id_boss WHERE u.id = ${idUsuario} GROUP BY u.nome, b.nome, b.tag ORDER BY nota DESC;
     `
     console.log('EXECUTANDO A INSTRUÇÃO SQL:  \n' + instrucaoSql);
     return database.executar(instrucaoSql)
@@ -23,7 +23,7 @@ function listar(idUsuario){
 function listarTodos(){
     console.log("ACESSEI O TIERLIST MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ");
     var instrucaoSql = `
-        SELECT b.nome as boss, ROUND(AVG(avat.nota), 1) as pontos, COUNT(avat.id_boss) as qtd_votos FROM bosses b JOIN avaliacoes_tierlist avat ON avat.id_boss = b.id GROUP BY b.nome ORDER BY pontos DESC, qtd_votos DESC;
+        SELECT b.nome as boss, b.tag, ROUND(AVG(avat.nota), 1) as pontos, COUNT(avat.id_boss) as qtd_votos FROM bosses b JOIN avaliacoes_tierlist avat ON avat.id_boss = b.id GROUP BY b.nome, b.tag ORDER BY pontos DESC, qtd_votos DESC;
     `
 
     console.log('EXECUTANDO A INSTRUÇÃO SQL:  \n' + instrucaoSql);
